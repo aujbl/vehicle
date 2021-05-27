b26e0f7327e29dd96a16cd6bfb37825f83b8d0f0
lee <aujblee@foxmail.com>
2021-05-27 17:52:26 +0800

    2021.5.27 17:52

--- licenseplate.py
+++ licenseplate.py
@@ -33,26 +33,32 @@ for c in j["config"]:
 	else:
 		raise RuntimeError('[ ERROR ] 没有设置有效配置参数.')
 
-blur = 3
+blur = 5
 img = cv2.GaussianBlur(img, (blur, blur), 0)
 oldimg = img
 img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-#开操作
-k = 7
-kernel = np.ones((k, k), np.uint8)
-img_opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
-# addWeighted: dst = src1*alpha + src2*beta + gamma;
-img_opening = cv2.addWeighted(img, 1, img_opening, -1, 0)
+# #开操作
+# k = 9
+# kernel = np.ones((k, k), np.uint8)
+# img_opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+# imshow(img_opening)
+# # addWeighted: dst = src1*alpha + src2*beta + gamma;
+# img_opening = cv2.addWeighted(img, 1, img_opening, -1, 0)
+# imshow(img_opening)
 
 # Canny边缘检测
-ret, img_thresh = cv2.threshold(img_opening, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+ret, img_thresh = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 img_edge = cv2.Canny(img_thresh, 100, 200)
 
 # 先闭后开运算
-kernel = np.ones((cfg['morphologyr'], cfg['morphologyc']), np.uint8)
+kernel_size = (7, 20)
+# kernel = np.ones((cfg['morphologyr'], cfg['morphologyc']), np.uint8)
+kernel = np.ones(kernel_size, np.uint8)
 img_edge = cv2.morphologyEx(img_edge, cv2.MORPH_CLOSE, kernel)
+imshow(img_edge)
 img_edge = cv2.morphologyEx(img_edge, cv2.MORPH_OPEN, kernel)
+imshow(img_edge)
 
 contours, _ = cv2.findContours(img_edge, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 # 筛选可能的区域轮廓
@@ -70,55 +76,55 @@ for contour in contours:
 		box = np.int0(box)
 		contour_img = cv2.drawContours(oldimg, [box], 0, (0, 0, 255), 2)
 imshow(contour_img)
-#
-# card_imgs = []
-# # 矩形区域可能是倾斜的矩形
-# for rect in car_rects:
-# 	angle = 1 if -1 < rect[2] < 1 else rect[2]
-# 	# 扩大rect范围，避免车牌边缘被排除
-# 	rect = (rect[0], (rect[1][0] + 5, rect[1][1] + 5), angle)
-#
-# 	box = cv2.boxPoints(rect)
-# 	# 避免边界超出图像边界
-# 	height_point = right_point = [0, 0]
-# 	left_point = low_point = [pic_width, pic_height]
-# 	for point in box:
-# 		if left_point[0] > point[0]:
-# 			left_point = point
-# 		if low_point[1] > point[1]:
-# 			low_point = point
-# 		if height_point[1] < point[1]:
-# 			height_point_point = point
-# 		if right_point[0] < point[0]:
-# 			right_point = point
-#
-# 	if left_point[1] <= right_point[1]:  # 正角度
-# 		new_right_point = [right_point[0], height_point[1]]
-# 		pts2 = np.float32([left_point, height_point, new_right_point])  # 字符只是高度需要改变
-# 		pts1 = np.float32([left_point, height_point, right_point])
-# 		M = cv2.getAffineTransform(pts1, pts2)  # 仿射变换
-# 		dst = cv2.warpAffine(oldimg, M, (pic_width, pic_height))
-# 		point_limit(new_right_point)
-# 		point_limit(height_point)
-# 		point_limit(left_point)
-# 		card_img = dst[int(left_point[1]):int(height_point[1]), int(left_point[0]):int(new_right_point[0])]
-# 		card_imgs.append(card_img)
-# 	# cv2.imshow("card", card_img)
-# 	# cv2.waitKey(0)
-# 	elif left_point[1] > right_point[1]:  # 负角度
-#
-# 		new_left_point = [left_point[0], height_point[1]]
-# 		pts2 = np.float32([new_left_point, height_point, right_point])  # 字符只是高度需要改变
-# 		pts1 = np.float32([left_point, height_point, right_point])
-# 		M = cv2.getAffineTransform(pts1, pts2)
-# 		dst = cv2.warpAffine(oldimg, M, (pic_width, pic_height))
-# 		point_limit(right_point)
-# 		point_limit(height_point)
-# 		point_limit(new_left_point)
-# 		card_img = dst[int(right_point[1]):int(height_point[1]), int(new_left_point[0]):int(right_point[0])]
-# 		card_imgs.append(card_img)
-# # cv2.imshow("card", card_img)
-# # cv2.waitKey(0)
+
+card_imgs = []
+# 矩形区域可能是倾斜的矩形
+for rect in car_rects:
+	angle = 1 if -1 < rect[2] < 1 else rect[2]
+	# 扩大rect范围，避免车牌边缘被排除
+	rect = (rect[0], (rect[1][0] + 5, rect[1][1] + 5), angle)
+	'''
+	.................*(top).................... - - >
+	| ........*(left)..*.......................
+	| .........*........*(right)..............
+	v ..........*(low).........................
+	左：x坐标最小，右：x坐标最大，顶：y坐标最小，底：y坐标最大
+	'''
+	box = cv2.boxPoints(rect)
+	# 按第一列排序
+	box = box[np.lexsort(box[:, ::-1].T)]
+	left_point, right_point = box[0], box[-1]
+	# 按最后一列排序
+	box = box[np.lexsort(box.T)]
+	height_point, low_point = box[0], box[-1]
+
+	if left_point[1] <= right_point[1]:  # 正角度
+		new_right_point = [right_point[0], height_point[1]]
+		pts2 = np.float32([left_point, height_point, new_right_point])  # 字符只是高度需要改变
+		pts1 = np.float32([left_point, height_point, right_point])
+		M = cv2.getAffineTransform(pts1, pts2)  # 仿射变换
+		dst = cv2.warpAffine(oldimg, M, (pic_width, pic_height))
+		point_limit(new_right_point)
+		point_limit(height_point)
+		point_limit(left_point)
+		card_img = dst[int(left_point[1]):int(height_point[1]), int(left_point[0]):int(new_right_point[0])]
+		card_imgs.append(card_img)
+	# cv2.imshow("card", card_img)
+	# cv2.waitKey(0)
+	elif left_point[1] > right_point[1]:  # 负角度
+
+		new_left_point = [left_point[0], height_point[1]]
+		pts2 = np.float32([new_left_point, height_point, right_point])  # 字符只是高度需要改变
+		pts1 = np.float32([left_point, height_point, right_point])
+		M = cv2.getAffineTransform(pts1, pts2)
+		dst = cv2.warpAffine(oldimg, M, (pic_width, pic_height))
+		point_limit(right_point)
+		point_limit(height_point)
+		point_limit(new_left_point)
+		card_img = dst[int(right_point[1]):int(height_point[1]), int(new_left_point[0]):int(right_point[0])]
+		card_imgs.append(card_img)
+# cv2.imshow("card", card_img)
+# cv2.waitKey(0)
 
 
 
